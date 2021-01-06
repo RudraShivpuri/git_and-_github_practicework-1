bc763cd4d39d69f749fea7b51a660e60b104e0a4
Rudra <rudrashivpuri@gmmail.com>
2021-01-06 13:01:20 +0530

    i have try to run the code for make the * complexicity

--- Tutorial8ex.c
+++ Tutorial8ex.c
@@ -8,14 +8,31 @@ enter the number you want multilication table of:
 output:
 table of 6:
 */
-int main(){
-int a ; 
-printf("enter the value you want to know the table \n",a);
-scanf("%d",&a);
-printf("the table of %d is\n",a);
+// int main(){
+// int a ; 
+// printf("enter the value you want to know the table \n",a);
+// scanf("%d",&a);
+// printf("the table of %d is\n",a);
 
-for(int n=1;n<=10;n++){
-    printf(" %d x %d = %d\n", a,n,a*n);
+// for(int n=1;n<=10;n++){
+//     printf(" %d x %d = %d\n", a,n,a*n);
+// }
+//        return 0;
+// }
+int main(){
+int rows , colloms ;
+
+printf("Enter the number of rows you want\n");
+scanf("%d",&rows);
+ printf("Enter the number of colloms you want \n");
+ scanf("%d",&colloms);
+for(int n=1; n<=rows;n++);
+{
+  for(int j=1;j<=colloms;j++){
+      printf("* ");
+  }
+  printf("\n");
 }
-       return 0;
+
+return 0;
 }
